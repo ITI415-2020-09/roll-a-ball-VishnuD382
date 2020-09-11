a7bdd563e522e23a96ad3636264161f5dfd4ba3f
VishnuD382 <vishnudhanasekaran9@gmail.com>
2020-09-11 15:39:52 -0400

    Finished 2.7

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.InputSystems;
 
 
 public class PlayerController : MonoBehaviour
 {
+
+    private RigidBody rb;
+    private float movementX;
+    private float movementY;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<RigidBody>();
     }
     
     void OnMove(InputValue movementValue)
     {
+        Vector2 movementVector = movementValue.get<Vector2>();
 
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement);
     }
 }
